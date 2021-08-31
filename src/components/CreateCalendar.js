2b7b422a24310e3a90609ec80645889ef20427cc
Emma Hammar <emmahammar@macbook-air.lan>
2021-08-31 19:45:38 +0200

    import npm react-calendar and add style

--- src/components/CreateCalendar.js
+++ src/components/CreateCalendar.js
@@ -1,6 +1,10 @@
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 function CreateCalendar() {
     return (
       <div>
+        <Calendar />
         <p>Skapa kalendern</p>
       </div>
     );
