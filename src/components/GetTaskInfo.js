4762aae2c8aca509a815b114e8a16245c5bfb948
Emma Hammar <emmahammar@macbook-air.lan>
2021-09-01 20:35:51 +0200

    testing onClickDay()

--- src/components/GetTaskInfo.js
+++ src/components/GetTaskInfo.js
@@ -3,6 +3,10 @@ const data = require('../data/taskInfo.json');
 function GetTaskInfo() {
 
     console.log(data);
+    
+    //jämföra klickade datumet med data[i].date i json:
+    //använda moment för att få ut year + month? 
+    console.log(data[0].date);
 
     return (
       <div>
