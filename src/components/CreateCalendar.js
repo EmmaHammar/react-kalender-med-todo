01de39b82ca24af599149fe9774beecd45e842c2
Emma Hammar <emmahammar@macbook-air.lan>
2021-09-01 11:28:45 +0200

    show dayCard when click on day

--- src/components/CreateCalendar.js
+++ src/components/CreateCalendar.js
@@ -1,6 +1,6 @@
 import Calendar from 'react-calendar';
 // import 'react-calendar/dist/Calendar.css';
-import { useState, setState } from 'react';
+import { useState, useEffect } from 'react';
 import PrintDayCard from './PrintDayCard';
 
 const moment = require('moment');
@@ -11,35 +11,46 @@ function CreateCalendar() {
   console.log("m", m.toString());
 
   const [value, onChange] = useState(new Date());
-  
-
-  const [day, setDay] = useState('');
 
   function handleChange(e) {
-    setDay(e);
-    console.log("e klickad dag:", e);
-
-    //printa DayCard - HUR?
-    // <PrintDayCard />
-  
-    
-    
-    let addTask = prompt( "lägg till ny uppgift för "+ e)
-    console.log("addTask:", addTask);
-    if (addTask !== "") {
-      alert ("Din uppgift är sparad");
-
-      
-    } else {
-      alert("Du måste fylla i en uppgift för att kunna spara.");
-    }
-    
-    
-    
-  }
-
+    // console.log("e klickad dag:", e);
+    printDayCard(e); //vill lägga över funktionen till en komponent, varför funkar det ej att skriva <PrintDayCard />?
+  };
 
   const tileContent = ({ date, view }) => view === 'month' && date.toDateString() === "Thu Aug 12 2021" ? <p>FÅNGAT ETT DATUM!</p> : null;
+  
+  const printDayCard = (e) => {
+    let dayCardBody = document.getElementById("dayCardBody");
+
+    //byta ut input till form med onSubmit:
+    dayCardBody.innerHTML = `
+      <div id="DayCardMain">
+        <div id="AddTaskCardContainer">
+          <h3>Lägg till ny uppgift för: ${e}</h3>
+          <input placeholder="Skriv din uppgift"></input>
+          <h4>Sätt deadline</h4>
+          <p>finns detta i react-calendar?</p>
+          <button>Spara</button>
+        </div>
+
+        <div id="DayListContainer">
+          <h3>Dagens lista</h3>
+          <ul>
+            <li>
+              <input type="checkbox">06:00 Vakna</input>
+            </li>
+            <li>
+              <input type="checkbox">07:30 Lämna barn</input>
+            </li>
+            <li>
+              <input type="checkbox">09:15 Möte</input>
+            </li>
+          </ul>
+        </div>
+      </div>
+    `;
+        
+  };
   
     return (
       <div>
@@ -49,15 +60,6 @@ function CreateCalendar() {
           onChange={ handleChange }
           // showWeekNumbers
           tileContent={ tileContent } 
-          // tileContent={ (date, view ) => (
-            
-          //   <div>
-          //     <div> 1 deadline </div>
-          //     <div> Task List </div>
-          //   </div>
-              
-          //   )} 
-
           value={ value } 
         />
         
@@ -78,3 +80,13 @@ function CreateCalendar() {
 
 // https://upmostly.com/tutorials/react-onchange-events-with-examples
 
+
+  //koden kommer köras varje gång komponent mountas o varje gång komponent uppdaterar sig:
+  // useEffect( (e) => {
+  //   console.log("useEffect");
+       
+  //   // return() => {};
+  
+  // }, []);
+  //laddas varje gång sidan laddas + klick (för att day omrenderas = då körs useEffect? Vill sen ändra så att useEffect endast körs vid klick)
+  //dependency-array, lämnas tom = bara köras onMount.  
--- src/components/PrintDayCard.js
+++ src/components/PrintDayCard.js
@@ -1,9 +1,35 @@
 function printDayCard() {
+  const dayCardBody = document.getElementById("dayCardBody");
+  
     return (
-      <div id="dayCardBody">
-        hej
-      </div>
-    );
+  
+      dayCardBody.innerHTML = `
+        <div id="DayCardMain">
+          <div id="AddTaskCardContainer">
+            <h3>Lägg till ny uppgift</h3>
+            <input placeholder="Skriv din uppgift"></input>
+            <h4>Sätt deadline</h4>
+            <p>finns detta i react-calendar?</p>
+            <button>Spara</button>
+          </div>
+
+          <div id="DayListContainer">
+            <h3>Dagens lista</h3>
+            <ul>
+              <li>
+                <input type="checkbox">06:00 Vakna</input>
+              </li>
+              <li>
+                <input type="checkbox">07:30 Lämna barn</input>
+              </li>
+              <li>
+                <input type="checkbox">09:15 Möte</input>
+              </li>
+            </ul>
+          </div>
+        </div>
+      `
+      );
   }
   
   export default printDayCard;
