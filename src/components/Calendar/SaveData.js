956fb0c6ccff9965f90d8edc46fcae27d5ef2cd7
Emma Hammar <emmahammar@84-216-155-84.customers.ownit.se>
2021-09-10 00:11:52 +0200

    change fetch-url to localhost and conclude that add task works locally, test change in backend repo

--- src/components/Calendar/SaveData.js
+++ src/components/Calendar/SaveData.js
@@ -1,6 +1,6 @@
 function SaveData(newTask) {
-    // fetch(`http://localhost:3012/task/add`, {
-    fetch(`https://react-kalender-med-to-do-be.herokuapp.com/task/add`, {
+    fetch(`http://localhost:3012/task/add`, {
+    // fetch(`https://react-kalender-med-to-do-be.herokuapp.com/task/add`, {
 
     method: 'post',
     headers: {
