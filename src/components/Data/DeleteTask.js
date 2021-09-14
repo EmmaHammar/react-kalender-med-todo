031db1ac43877e1781679fc09c29cde7fd32cab6
EmmaHammar <emma.s.hammar@gmail.com>
2021-09-14 22:52:42 +0200

    removing s

--- src/components/Data/DeleteTask.js
+++ src/components/Data/DeleteTask.js
@@ -2,7 +2,7 @@ function DeleteTask(taskId) {
     // console.log("taskId i DeleteTask.js", taskId);
 
     // fetch(`http://localhost:3012/delete`, {
-    fetch(`https://react-kalender-med-to-do-be.herokuapp.com/delete`, {
+    fetch(`http://react-kalender-med-to-do-be.herokuapp.com/delete`, {
 
         method: 'post',
         headers: {
